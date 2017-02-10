04329840b005ef27a44383eaf30d733a831fa7a8
SamLindley <samlindley0@gmail.com>
2017-02-10 11:22:31 +0100

    created table avdaslfkdslgkjsdlk

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -4,10 +4,10 @@ USE auktionssystem;
 
 -- adress
 CREATE TABLE adress(
-id INT AUTO_INCREMENT PRIMARY KEY,
-gata VARCHAR (50) NOT NULL,
-postnummer CHAR (5) NOT NULL,
-ort VARCHAR (50)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  gata VARCHAR (50) NOT NULL,
+  postnummer CHAR (5) NOT NULL,
+  ort VARCHAR (50)
 );
 
 -- kund
@@ -49,3 +49,17 @@ CREATE TABLE produkt (
 -- bud
 
 -- avslutade auktioner
+CREATE TABLE avslutade_auktioner
+(
+  id INT NOT NULL ,
+  produkt_id INT,
+  hogsta_bud DOUBLE,
+  kund_personnummer CHAR(10),
+  startdatum DATE,
+  slutdatum DATE,
+  utgangspris DOUBLE,
+  acceptpris DOUBLE,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (produkt_id) REFERENCES produkt (produkt_id)
+);
