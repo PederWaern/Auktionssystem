ec974960263b0c46b0acf42162e21593b34eebfc
VincentXis <rickard.bernal@gmail.com>
2017-02-10 14:05:19 +0100

    la till timestamp på bud

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -61,6 +61,7 @@ CREATE TABLE bud (
   kund_personnummer CHAR(10) NOT NULL,
   auktion_id INT NOT NULL,
   belopp double NOT NULL ,
+  tid DATETIME DEFAULT current_timestamp,
   PRIMARY KEY (kund_personnummer, auktion_id),
   FOREIGN KEY (kund_personnummer) REFERENCES kund (personnummer),
   FOREIGN KEY (auktion_id) REFERENCES auktion (id)
