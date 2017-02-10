23c86fd9d962e950ab51e51b186835786b795a5f
daniellindkarlberg <danielskua@hotmail.com>
2017-02-10 11:00:20 +0100

    added table kund

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -4,7 +4,16 @@ USE auktionssystem;
 
 -- adress
 
--- kund
+CREATE TABLE kund(
+  personnummer INT(10),
+  fornamn       VARCHAR(50) NOT NULL,
+  efternamn     VARCHAR(50) NOT NULL,
+  telefonnummer CHAR(13)    NOT NULL,
+  epost         VARCHAR(50) NOT NULL,
+  adress_id     INT,
+  PRIMARY KEY (personnummer),
+  FOREIGN KEY (adress_id) REFERENCES adress (id)
+);
 
 -- leverantor
 
