49a99d67ae98b3bb814f97b63630899da713bdda
SamLindley <samlindley0@gmail.com>
2017-02-10 11:09:33 +0100

    creted table leverantooooooooooooooooooor

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -7,7 +7,15 @@ USE auktionssystem;
 -- kund
 
 -- leverantor
+CREATE TABLE leverantor
+(
+  organisitions_nummer CHAR(12) NOT NULL ,
+  name VARCHAR(50),
+  telefon_nummer VARCHAR(13),
+  epost VARCHAR(50),
 
+  PRIMARY KEY (organisitions_nummer)
+)
 -- produkt
 
 -- auktion
