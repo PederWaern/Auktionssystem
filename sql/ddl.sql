e5e7e2dd0b3d1d81e9f1afc2bfea02fa397dfb1f
Christopher <christopher.olsson@hyperisland.se>
2017-02-10 11:06:03 +0100

    added adress

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -3,6 +3,12 @@ CREATE DATABASE auktionssystem;
 USE auktionssystem;
 
 -- adress
+CREATE TABLE adress(
+id INT AUTO_INCREMENT PRIMARY KEY,
+gata VARCHAR (50) NOT NULL,
+postnummer CHAR (5) NOT NULL,
+ort VARCHAR (50)
+);
 
 -- kund
 
