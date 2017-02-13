c96c6cd08ef1b6856e4afaf6a41d4036684c049c
SamLindley <samlindley0@gmail.com>
2017-02-13 12:54:55 +0100

    basic structure

--- auktionssystem/src/com/surperfluousfew/auktionsystem/controllers/HomeController.java
+++ auktionssystem/src/com/surperfluousfew/auktionsystem/controllers/HomeController.java
@@ -1,5 +1,27 @@
 package com.surperfluousfew.auktionsystem.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class HomeController {
+
+    public void initialize(){
+    }
+
+    @FXML
+    Button bPagaendeAuktioner;
+
+    public void setScenePGA(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/addAuktion.fxml"));
+
+
+    }
+
 
 }
