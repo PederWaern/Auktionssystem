223043857427dc5b70b23123ac3f57f6c3b52cbd
SamLindley <samlindley0@gmail.com>
2017-02-14 13:55:32 +0100

    basic structure

--- auktionssystem/src/com/surperfluousfew/auktionsystem/controllers/AddAuktionController.java
+++ auktionssystem/src/com/surperfluousfew/auktionsystem/controllers/AddAuktionController.java
@@ -1,18 +1,37 @@
 package com.surperfluousfew.auktionsystem.controllers;
 
 import com.surperfluousfew.auktionsystem.StageHandler;
+import com.surperfluousfew.auktionsystem.models.Auktion;
+import com.surperfluousfew.auktionsystem.models.Produkt;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class AddAuktionController {
 
-    Stage stage;
-    StageHandler stageHandler = new StageHandler();
+    ArrayList<Auktion> pagaendeAuktioner = new ArrayList<>();
+    ArrayList<Produkt> produkts = new ArrayList<>();
 
     @FXML
     Parent root;
+    @FXML
+    ChoiceBox cbProdukt;
+    @FXML
+    TextField txfUpris;
+    @FXML
+    TextField txfApris;
+    @FXML
+    DatePicker dpStart;
+    @FXML
+    DatePicker dpSlut;
+
+
 }
---
+++ auktionssystem/src/com/surperfluousfew/auktionsystem/controllers/ForsaljningsstatistikController.java
@@ -0,0 +1,23 @@
+package com.surperfluousfew.auktionsystem.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+
+public class ForsaljningsstatistikController {
+    @FXML
+    BorderPane root;
+
+    public void showKunder(ActionEvent actionEvent) {
+
+    }
+
+    public void showAuktioner(ActionEvent actionEvent) {
+
+    }
+
+    public void showProvision(ActionEvent actionEvent) {
+
+    }
+}
