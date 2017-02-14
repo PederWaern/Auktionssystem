f03dcba48e3382143089b709775cc704481eb418
SamLindley <samlindley0@gmail.com>
2017-02-14 12:02:03 +0100

    basic structure

--- auktionssystem/src/com/surperfluousfew/auktionsystem/controllers/AddKundController.java
+++ auktionssystem/src/com/surperfluousfew/auktionsystem/controllers/AddKundController.java
@@ -1,17 +1,40 @@
 package com.surperfluousfew.auktionsystem.controllers;
 
-import com.surperfluousfew.auktionsystem.StageHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 
 public class AddKundController {
 
+
+
+
+
     @FXML
     Parent root;
+    @FXML
+    TextField txfFirstName;
+    @FXML
+    TextField txfLastName;
+    @FXML
+    TextField txfPnummer;
+    @FXML
+    TextField txfEpost;
+    @FXML
+    TextField txfTelnummer;
+    @FXML
+    TextField txfGata;
+    @FXML
+    TextField txfPostnummer;
+    @FXML
+    TextField txfOrt;
+    @FXML
+    Button bAddKund;
 
+    public void addKund(ActionEvent actionEvent) {
 
+    }
 }
