bb6ace1e710031c21d7a70d14b80190edd65b73f
Christopher <christopher.olsson@hyperisland.se>
2017-02-14 11:01:56 +0100

    added admin

--- sql/dml.sql
+++ sql/dml.sql
@@ -9,25 +9,30 @@ INSERT INTO adress (gata, postnummer, ort) VALUES
   ('Högbergs gatan 7', 10562, 'Lågdalen'),
   ('Kvadratvägen 55', 14895, 'Plankholm'),
   ('Valör gatan 100', 16892, 'Njutingö'),
-  ('Omvägen 69', 19812, 'Nedsala');
+  ('Omvägen 69', 19812, 'Nedsala'),
+  ('Östermalmstorg 53', 54634, 'Stockholm');
 
 -- kunder
-INSERT INTO kund (personnummer, fornamn, efternamn, telefonnummer, epost, losenord, adress_id) VALUES
-  ('6808033117', 'Fritte', 'Bohman', '07374826', 'frittw.bohman@domäinen.de', '111', 1),
-  ('3212077743', 'Anna', 'Lund', '0743782644', 'hacker.c8s@anon.w', '222', 2),
-  ('8707736734', 'Noppe', 'Segelbåt', '0798375892', 'noppe.segelbåt@buissenes.com', '333', 3),
-  ('7309824728', 'Limpan', 'Persson', '0734683844', 'limpan123.ha@hotmail.com', '444', 4),
-  ('5503047294', 'Edit', 'Gärdeström', '0794782828', 'vadsadu@virus.com', '555', 5),
-  ('7706034568', 'Bella', 'Bortskämd', '0783672837', 'Bellam@bloggen.se', '666', 6);
+INSERT INTO kund (personnummer, fornamn, efternamn, telefonnummer, epost, adress_id) VALUES
+  ('6808033117', 'Fritte', 'Bohman', '0737482653', 'frittw.bohman@domäinen.de',  1),
+  ('3212077743', 'Anna', 'Lund', '0743782644', 'hacker.c8s@anon.w', 2),
+  ('8707736734', 'Noppe', 'Segelbåt', '0798375892', 'noppe.segelbåt@buissenes.com', 3),
+  ('7309824728', 'Limpan', 'Persson', '0734683844', 'limpan123.ha@hotmail.com',  4),
+  ('5503047294', 'Edit', 'Gärdeström', '0794782828', 'vadsadu@virus.com', 5),
+  ('7706034568', 'Bella', 'Bortskämd', '0783672837', 'Bellam@bloggen.se',  6);
+
+  -- admins
+INSERT INTO admin (personnummer, fornamn, efternamn, telefonnummer, epost, losenord, adress_id) VALUES
+  ('1703052157', 'Gunnar', 'Tillhamre', '0738463987', 'gunnar.tillhamre@domäinen.de', '111', 7);
 
 -- leverantorer
-INSERT INTO leverantor (organisitionsnummer, telefonnummer, epost, losenord, namn, provision) VALUES
-  ('111111111111', '0735111111', 'los@sell.se', '111', 'Lovely Old Stuff', 0.3),
-  ('222222222222', '0735222222', 'hs@sell.se', '222', 'Happy Shop', 0.1),
-  ('333333333333', '0735333333', 'ems@sell.se', '333', 'Evil Megastore', 0.5),
-  ('444444444444', '0735444444', 'fod@sell.se', '444', 'Friendly Old Dude', 0.25),
-  ('555555555555', '0735555555', 'msb@sell.se', '555', 'Ms. Butterscotch', 0.35),
-  ('666666666666', '0735666666', 'wgtg@sell.se', '666', 'We Got The Goods', 0.1);
+INSERT INTO leverantor (organisitionsnummer, telefonnummer, epost, namn, provision) VALUES
+  ('111111111111', '0735111111', 'los@sell.se', 'Lovely Old Stuff', 0.3),
+  ('222222222222', '0735222222', 'hs@sell.se', 'Happy Shop', 0.1),
+  ('333333333333', '0735333333', 'ems@sell.se', 'Evil Megastore', 0.5),
+  ('444444444444', '0735444444', 'fod@sell.se', 'Friendly Old Dude', 0.25),
+  ('555555555555', '0735555555', 'msb@sell.se', 'Ms. Butterscotch', 0.35),
+  ('666666666666', '0735666666', 'wgtg@sell.se', 'We Got The Goods', 0.1);
 
 -- produkter
 INSERT INTO produkt (leverantor_organisationsnummer, namn, beskrivning, bildnamn) VALUES
@@ -73,11 +78,11 @@ FROM auktion;
 
 -- registrera en produkt
   INSERT INTO produkt (leverantor_organisationsnummer, namn, beskrivning, bildnamn) VALUES
-  ('?','?','?');
+  ('?','?','?', '?');
 
  -- lägg till auktion
 
-call lägg_till_auktion('?','?','?','?','?','?','?');
+call lägg_till_auktion ('?','?','?','?','?','?','?');
 
 -- lista pågående auktioner
 
@@ -106,4 +111,3 @@ SELECT * from provision_per_manad;
 
 
 
-  
