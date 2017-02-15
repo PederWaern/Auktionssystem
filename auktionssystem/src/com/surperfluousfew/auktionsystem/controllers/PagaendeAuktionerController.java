a46fee98e6c9393febd83856b7afd91e9dd261c9
anNe13 <rtr.13@hotmail.com>
2017-02-15 11:05:43 +0100

    Budhistorik inte klar men påbörjad

--- auktionssystem/src/com/surperfluousfew/auktionsystem/controllers/PagaendeAuktionerController.java
+++ auktionssystem/src/com/surperfluousfew/auktionsystem/controllers/PagaendeAuktionerController.java
@@ -1,25 +1,62 @@
 package com.surperfluousfew.auktionsystem.controllers;
 
+import com.surperfluousfew.auktionsystem.DatabaseLoader;
 import com.surperfluousfew.auktionsystem.StageHandler;
+import com.surperfluousfew.auktionsystem.models.Bud;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+
+import java.util.List;
 
 public class PagaendeAuktionerController {
 
-    StageHandler stageHandler = new StageHandler();
     Stage stage;
+    StageHandler stageHandler = new StageHandler();
 
     @FXML
     Parent root;
+    @FXML
+    ChoiceBox cbAuktion;
+    @FXML
+    ListView listView;
 
-    public void goBack(ActionEvent actionEvent) throws Exception {
-        Parent homeScreen = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
-        stage = stageHandler.getParentStage(root);
-        stage.setScene(new Scene(homeScreen));
+
+    @FXML
+    public void initialize(){
+      //  populateView();
+        System.out.println("inne i initialize");
     }
+    public void populateView() {
 
+        DatabaseLoader dbLoader = new DatabaseLoader();
+        dbLoader.loadAddresses();
+        dbLoader.loadLeverantor();
+        dbLoader.loadKund();
+        dbLoader.loadBud();
+        List<Bud> budList = dbLoader.getBud();
+
+
+        ObservableList<String> items = FXCollections.observableArrayList ();
+        String förnamn, efternamn, personnummer, belopp, tid;
+        for (Bud b : budList) {
+            //if (b.getAuktion().getId() == Integer.parseInt(cbAuktion.getValue().toString()))
+            {
+                förnamn = b.getKund().getFornamn();
+                efternamn = b.getKund().getEfternamn();
+                personnummer = b.getKund().getPersonnummer();
+                belopp = String.valueOf(b.getBelopp());
+                tid = b.getTid();
+                items.add(personnummer + " "  +förnamn + " " + efternamn + "\n" + tid + "   " + belopp + ":-");}
+        }
+
+        listView.setItems(items);
+    }
 }
