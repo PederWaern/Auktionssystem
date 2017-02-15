1812da062d8cec76e0f252f60e0497e3a81832c4
SamLindley <samlindley0@gmail.com>
2017-02-15 12:06:37 +0100

    new Class

--- auktionssystem/src/com/surperfluousfew/auktionsystem/controllers/AuktionTidsintervallController.java
+++ auktionssystem/src/com/surperfluousfew/auktionsystem/controllers/AuktionTidsintervallController.java
@@ -1,7 +1,4 @@
 package com.surperfluousfew.auktionsystem.controllers;
 
-/**
- * Created by daniellindkarlberg on 2017-02-15.
- */
 public class AuktionTidsintervallController {
 }
--- auktionssystem/src/com/surperfluousfew/auktionsystem/models/AuktionTidsintervall.java
+++ auktionssystem/src/com/surperfluousfew/auktionsystem/models/AuktionTidsintervall.java
@@ -7,5 +7,36 @@ public class AuktionTidsintervall {
 
     private String produktNamn;
     private String slutDatum;
-    private
+    private double hogstaBud;
+    private double beraknadProvision;
+    private String provandel;
+
+    public AuktionTidsintervall(String produktNamn, String slutDatum, double hogstaBud, double beraknadProvision, String provandel) {
+        this.produktNamn = produktNamn;
+        this.slutDatum = slutDatum;
+        this.hogstaBud = hogstaBud;
+        this.beraknadProvision = beraknadProvision;
+        this.provandel = provandel;
+    }
+
+    public String getProduktNamn() {
+        return produktNamn;
+    }
+
+    public String getSlutDatum() {
+        return slutDatum;
+    }
+
+    public double getHogstaBud() {
+        return hogstaBud;
+    }
+
+    public double getBeraknadProvision() {
+        return beraknadProvision;
+    }
+
+    public String getProvandel() {
+        return provandel;
+    }
+
 }
