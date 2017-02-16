205a339953f7a74adbd8391a9b6c424a974cbfa5
daniellindkarlberg <danielskua@hotmail.com>
2017-02-16 11:39:37 +0100

    added class auktionstidsintervall class

--- auktionssystem/src/com/surperfluousfew/auktionsystem/DatabaseLoader.java
+++ auktionssystem/src/com/surperfluousfew/auktionsystem/DatabaseLoader.java
@@ -329,7 +329,7 @@ public class DatabaseLoader {
                String personNummer = resultSet.getString(3);
                double totaltOrderVärde = resultSet.getDouble(4);
 
-               list.add(new TotalOrderVärdePerKund(fornamn,efternamn,personNummer, totaltOrderVärde));
+               list.add(new TotalOrderVärdePerKund(fornamn,efternamn,personNummer, totaltOrderVärde + " kr"));
 
             }
 
@@ -427,8 +427,8 @@ public class DatabaseLoader {
             while (resultSet.next()){
                 list.add(new AuktionTidsintervall(resultSet.getString(1),
                         resultSet.getString(7),
-                        resultSet.getDouble(8),
-                        resultSet.getDouble(9),
+                        resultSet.getDouble(8) + " kr",
+                        resultSet.getDouble(9) + " kr",
                         resultSet.getString(10)));
 
             }
